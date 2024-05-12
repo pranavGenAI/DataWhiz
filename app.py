b53178797d9044366fe114d3d6524d45397fde0e
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-13 02:57:33 +0530

    app.py

--- app.py
+++ app.py
@@ -1,78 +1,72 @@
-import streamlit as st 
-from streamlit_chat import message
-import tempfile
+import streamlit as st
+import os
 from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.llms import CTransformers
 from langchain.chains import ConversationalRetrievalChain
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import google.generativeai as genai
 
-DB_FAISS_PATH = 'vectorstore/db_faiss'
-api_key = st.secrets['GEMINI_API_KEY']
-#Loading the model
-def load_llm():
-    # Load the locally downloaded model here
-    llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3, google_api_key=api_key)
-    return llm
 
-st.title("Chat with CSV using Llama2 🦙🦜")
-st.markdown("<h3 style='text-align: center; color: white;'>Built by <a href='https://github.com/AIAnytime'>AI Anytime with ❤️ </a></h3>", unsafe_allow_html=True)
+def add_vertical_space(spaces=1):
+    for _ in range(spaces):
+        st.sidebar.markdown("---")
 
-uploaded_file = st.sidebar.file_uploader("Upload your Data", type="csv")
+def main():
+    st.set_page_config(page_title="DataWhiz 📊")
+    st.title("DataWhiz Chatbot 📊💬")
+    st.subheader("Your personal data analyst! 👨🏻‍💻")
 
-if uploaded_file :
-   #use tempfile because CSVLoader only accepts a file_path
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(uploaded_file.getvalue())
-        tmp_file_path = tmp_file.name
+    st.sidebar.title("DataWhiz")
 
-    loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={
-                'delimiter': ','})
-    data = loader.load()
-    #st.json(data)
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
-                                       model_kwargs={'device': 'auto'})
 
-    db = FAISS.from_documents(data, embeddings)
-    db.save_local(DB_FAISS_PATH)
-    llm = load_llm()
-    chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=db.as_retriever())
+    DB_FAISS_PATH = "vectorstore/db_faiss"
+    TEMP_DIR = "temp"
 
-    def conversational_chat(query):
-        result = chain({"question": query, "chat_history": st.session_state['history']})
-        st.session_state['history'].append((query, result["answer"]))
-        return result["answer"]
-    
-    if 'history' not in st.session_state:
-        st.session_state['history'] = []
+    if not os.path.exists(TEMP_DIR):
+        os.makedirs(TEMP_DIR)
 
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["Hello ! Ask me anything about " + uploaded_file.name + " 🤗"]
+    uploaded_file = st.sidebar.file_uploader("Upload CSV file", type=['csv'])
 
-    if 'past' not in st.session_state:
-        st.session_state['past'] = ["Hey ! 👋"]
-        
-    #container for the chat history
-    response_container = st.container()
-    #container for the user's text input
-    container = st.container()
+    add_vertical_space(1)
+    st.sidebar.write('Accenture S&C GN H&PS')
 
-    with container:
-        with st.form(key='my_form', clear_on_submit=True):
-            
-            user_input = st.text_input("Query:", placeholder="Talk to your csv data here (:", key='input')
-            submit_button = st.form_submit_button(label='Send')
-            
-        if submit_button and user_input:
-            output = conversational_chat(user_input)
-            
-            st.session_state['past'].append(user_input)
-            st.session_state['generated'].append(output)
+    if uploaded_file is not None:
+        file_path = os.path.join(TEMP_DIR, uploaded_file.name)
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getvalue())
 
-    if st.session_state['generated']:
-        with response_container:
-            for i in range(len(st.session_state['generated'])):
-                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-                message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+        st.write(f"Uploaded file: {uploaded_file.name}")
+        st.write("Processing CSV file...")
+
+        loader = CSVLoader(file_path=file_path, encoding="utf-8", csv_args={'delimiter': ','})
+        data = loader.load()
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+        text_chunks = text_splitter.split_documents(data)
+
+        st.write(f"Total text chunks: {len(text_chunks)}")
+
+        embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+        docsearch = FAISS.from_documents(text_chunks, embeddings)
+        docsearch.save_local(DB_FAISS_PATH)
+
+        llm = CTransformers(model="C:/Users/pranav.baviskar/Desktop/Learning/GenAI/Datawhiz/models/llama-2-7b-chat.ggmlv3.q4_0.bin",
+                            model_type="llama",
+                            max_new_tokens=512,
+                            temperature=0.1)
+
+        qa = ConversationalRetrievalChain.from_llm(llm, retriever=docsearch.as_retriever())
+
+        st.write("Enter your query:")
+        query = st.text_input("Input Prompt:")
+        if query:
+            with st.spinner("Processing your question..."):
+                chat_history = []
+                result = qa({"question": query, "chat_history": chat_history})
+                st.write("Response:", result['answer'])
+
+        os.remove(file_path)
+
+if __name__ == "__main__":
+    main()
