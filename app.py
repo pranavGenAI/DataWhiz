329b5ceaa01cba03226774ac3b72de445960c8f7
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-13 02:57:59 +0530

    app.py

--- app.py
+++ app.py
@@ -46,8 +46,7 @@ def main():
         text_chunks = text_splitter.split_documents(data)
 
         st.write(f"Total text chunks: {len(text_chunks)}")
-
-        embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+        embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
         docsearch = FAISS.from_documents(text_chunks, embeddings)
         docsearch.save_local(DB_FAISS_PATH)
 
