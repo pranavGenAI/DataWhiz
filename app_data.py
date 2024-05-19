47ea28d122636c7507a720742b757be8464e91c9
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-19 20:02:45 +0530

    app_data.py

--- app_data.py
+++ app_data.py
@@ -55,7 +55,7 @@ st.markdown('''<style>
         background-color: transparent;
     }
     .stApp {
-        background: linear-gradient(45deg, #FFFFFF 60%, #FAE9D3 40%, #e7abb4 80%);
+        background: linear-gradient(45deg, #FFFFFF 60%, #FAE9D3 20%, #e7abb4 20%);
         animation: my_animation 20s ease infinite;
         background-size: 200% 200%;
         background-attachment: fixed;
